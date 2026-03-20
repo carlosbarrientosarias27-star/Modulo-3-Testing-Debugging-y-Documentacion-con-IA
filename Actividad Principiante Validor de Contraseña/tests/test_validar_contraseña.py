f1cad16b62888509fe78ce347f8e5a4103f7f8e8
carlosbarrientosarias27-star <carlos.barrientosarias27@gmail.com>
2026-03-20 10:28:00 +0100

    cambios y tests

---
+++ Actividad Principiante Validor de Contraseña/tests/test_validar_contraseña.py
@@ -0,0 +1,23 @@
+import pytest
+from auth import validar_contrasena
+# Test 1
+def test_contrasena_valida():
+# Arrange
+    contrasena = "Segura1!"
+# Act
+    resultado = validar_contrasena(contrasena)
+# Assert
+    assert resultado == True
+# Test 2
+def test_sin_mayuscula():
+    assert validar_contrasena("segura1!") == False 
+
+# Test 3
+def test_sin_numero():
+    assert validar_contrasena("Seguridad!") == False
+# Test 4
+def test_exactamente_8_caracteres():
+    assert validar_contrasena("Segur1!a") == True
+# Test 5
+def test_con_espacio():
+    assert validar_contrasena("Segura 1!") == False
